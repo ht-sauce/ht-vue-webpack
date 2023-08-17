5c5ed850a8972ab61713924e81c2cbd47be9b628
dht <603928588@qq.com>
2023-08-17 17:40:20 +0800

    基本改动完成

--- plugin/preview/serve.js
+++ plugin/preview/serve.js
@@ -1,29 +1,19 @@
 const express = require('express')
-const webpack = require('webpack')
-const webpackDevMiddleware = require('webpack-dev-middleware')
 const proxy = require('http-proxy-middleware').createProxyMiddleware // 代理处理
 const app = express()
 const path = require('path')
+const fs = require('fs')
 
-console.log(process.cwd())
-const config = require(path.join(process.cwd(), 'webpack.config.js'))
-
-const compiler = webpack(config)
-
-// 告知 express 使用 webpack-dev-middleware，
-// 以及将 webpack.config.js 配置文件作为基础配置。
-
-//express框架，前边肯定都是必要的，也就是只需安装compression组件，然后添加一下两句代码就好
-const compression = require('compression')
-app.use(compression())
-
-app.use(
-  webpackDevMiddleware(compiler, {
-    publicPath: config.output.publicPath, // 解决图标路径问题
-  }),
-)
 // 代理配置
 // app.use('/api', proxy({ target: 'http://localhost:8080', changeOrigin: true }))
+
+console.log(path.join(process.cwd(), '/dist/index.html'))
+// 访问静态资源
+app.use(express.static(path.join(process.cwd(), '/dist')))
+app.get('*', function (req, res) {
+  const html = fs.readFileSync(path.join(process.cwd(), '/dist/index.html'), 'utf-8')
+  res.send(html)
+})
 
 const port = 3000
 // 将文件 serve 到 port 3000。
