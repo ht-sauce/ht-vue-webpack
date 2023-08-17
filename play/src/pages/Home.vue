1e73ea06e8c3cb377f6e584b51cf880746b876e4
dht <603928588@qq.com>
2023-08-17 16:26:11 +0800

    增加一些规范处理

--- play/src/pages/Home.vue
+++ play/src/pages/Home.vue
@@ -5,6 +5,9 @@
     <router-link to="/test">测试页面2</router-link>
   </div>
   <img class="img-style" src="@/assets/1689044096720.jpg" alt="" />
+  <div v-for="item in [1, 2, 3]" :key="item">
+    <div>{{ item }}</div>
+  </div>
 </template>
 
 <style scoped lang="scss">
