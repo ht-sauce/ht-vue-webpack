a3a16e06d1c800e03c40279d128d768cbc65c234
dht <603928588@qq.com>
2023-08-24 15:53:33 +0800

    生产构建增加gzip,调整

--- plugin/config/webpack-prd.js
+++ plugin/config/webpack-prd.js
@@ -9,7 +9,7 @@ module.exports = function (cliOptions) {
     plugins: [
       new CompressionPlugin({
         // filename: "[path][base].gz", // 这种方式是默认的，多个文件压缩就有多个.gz文件，建议使用下方的写法
-        filename: '[path].gz[query]', //  使得多个.gz文件合并成一个文件，这种方式压缩后的文件少，建议使用
+        // filename: '[path].gz[query]', //  使得多个.gz文件合并成一个文件，这种方式压缩后的文件少，建议使用
         algorithm: 'gzip', // 官方默认压缩算法也是gzip
         test: /\.js$|\.css$|\.html$|\.ttf$|\.eot$|\.woff$/, // 使用正则给匹配到的文件做压缩，这里是给html、css、js以及字体（.ttf和.woff和.eot）做压缩
         threshold: 10240, //以字节为单位压缩超过此大小的文件，使用默认值10240吧
